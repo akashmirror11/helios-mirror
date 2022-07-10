d1d519afcb712cddc7310d970f0f67b9e9e8c393
akashmirror11 <105482805+akashmirror11@users.noreply.github.com>
2022-07-10 21:23:42 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -54,8 +54,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("Owner", "https://t.me/HELL_GaM")
+    buttons.buildbutton("Mirror Group", "https://t.me/+5D_ikTuvUa8xZDZl")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
